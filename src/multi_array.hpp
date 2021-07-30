81eeba40c35077325cac8885b49ee320f1ecb694
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2021-07-30 10:46:30 +0200

    Fix portability issues with std::size_t
    Use the namespaced version of std::size_t. The plain size_t
    usually comes from the deprecated header <stddef.h> and can lead
    to build failures on recent compilers if <cstddef> is not included.

--- src/multi_array.hpp
+++ src/multi_array.hpp
@@ -61,7 +61,7 @@ struct all<> {
 
 // linearized_index
 
-template <size_t n, size_t... N>
+template <std::size_t n, std::size_t... N>
 struct linearized_index {
     template <typename... Idx>
     DEVICE_FUNC constexpr std::size_t operator()(Idx... idx) const noexcept {
@@ -72,7 +72,7 @@ struct linearized_index {
     }
 };
 
-template <size_t... N>
+template <std::size_t... N>
 struct linearized_index<0, N...> {
     template <typename... Idx>
     DEVICE_FUNC constexpr std::size_t operator()(Idx... idx) const noexcept {
@@ -83,7 +83,7 @@ struct linearized_index<0, N...> {
 
 // check_bounds
 
-template <size_t n, size_t... N>
+template <std::size_t n, std::size_t... N>
 struct check_bounds {
     template <typename... Idx>
     constexpr bool operator()(Idx... idx) const {
@@ -95,7 +95,7 @@ struct check_bounds {
     }
 };
 
-template <size_t... N>
+template <std::size_t... N>
 struct check_bounds<0, N...> {
     template <typename... Idx>
     constexpr bool operator()(Idx... idx) const {
